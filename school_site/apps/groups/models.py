8ed99b6d6b554705fd036b367c4e40c1e6cceafa
So-Andrey <and_rey_s@inbox.ru>
2025-05-09 20:42:33 +0300

    fix: non-nullable teacher_id fixed

--- school_site/apps/groups/models.py
+++ school_site/apps/groups/models.py
@@ -18,8 +18,7 @@ class Group(Base, TimestampMixin):
     id = Column(PostgresUUID(as_uuid=True), primary_key=True, default=uuid4)
     name = Column(String, unique=True, nullable=False)
     description = Column(String, nullable=False)
-    teacher_id = Column(PostgresUUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    teacher_id = Column(PostgresUUID(as_uuid=True), ForeignKey("users.id"), nullable=True)
 
-    # Связи
     teacher = relationship("User", back_populates="groups")
     students = relationship("Student", secondary=group_student, back_populates="groups")
