8f1aecfc95b7dc0e82dfb48fcb93247f52949c3e
So-Andrey <and_rey_s@inbox.ru>
2025-06-04 19:58:29 +0300

    fix: fix access_token getting in teachers router

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -2,4 +2,4 @@ uv run python -m scripts.create_user --password 1 --role admin --first_name Pave
 
 uv run python -m scripts.create_user --password 1 --role student --first_name Pavel --surname Kozhinov --patronymic Sergeevich --email pavel.seko4@gmail.com --phone_number +79642501606 --points 200
 
- uv run python -m scripts.create_user --password 1 --role teacher --first_name Pavel --surname Kozhinov --patronymic Sergeevich --email p12113avel.seko6@gmail.com --phone_number +79648701406
+uv run python -m scripts.create_user --password 1 --role teacher --first_name Pavel --surname Kozhinov --patronymic Sergeevich --email p12113avel.seko6@gmail.com --phone_number +79648701406
