57eb3b7d7a9b011bc08bb2281a6e91f8419f9776
anthocyane <anthocyane@yandex.ru>
2025-07-14 17:35:55 +0300

    fix: fix update users

--- school_site/apps/users/services/permissions.py
+++ school_site/apps/users/services/permissions.py
@@ -1,5 +1,6 @@
 from typing import Self
-from school_site.apps.users.models import User, UserRole
+from school_site.core.enums import UserRole
+from school_site.apps.users.schemas import UserTokenDataReadSchema
 from school_site.core.utils.exceptions import PermissionDeniedError
 
 
@@ -14,12 +15,12 @@ class PermissionsService:
             "delete_user": self._check_delete_user,
         }
     
-    def check(self: Self, operation: str, current_user: User, **context) -> bool:
+    def check(self: Self, operation: str, current_user: UserTokenDataReadSchema, **context) -> bool:
         if operation not in self.operations:
             raise ValueError(f"Unknown permission operation: {operation}")
         return self.operations[operation](current_user, **context)
 
-    def _check_get_user(self: Self, current_user: User, **context) -> bool:
+    def _check_get_user(self: Self, current_user: UserTokenDataReadSchema, **context) -> bool:
         target_user = context.get('target_user')
         if not target_user:
             raise ValueError("Target user is required for get_user check")
@@ -28,19 +29,36 @@ class PermissionsService:
             raise PermissionDeniedError()
         return True
     
-    def _check_get_all_users(self: Self, current_user: User, **context) -> bool:
+    def _check_get_all_users(self: Self, current_user: UserTokenDataReadSchema, **context) -> bool:
         return current_user.role == UserRole.ADMIN
     
-    def _check_update_user(self: Self, current_user: User, **context) -> bool:
+    def _check_update_user(self: Self, current_user: UserTokenDataReadSchema, **context) -> bool:
         target_user = context.get('target_user')
+        target_user_id = context.get('target_user_id')
+        
         if not target_user:
             raise ValueError("Target user is required for update_user check")
         
-        if current_user.role == UserRole.ADMIN and target_user.role != UserRole.STUDENT:
+        # Проверка прав для разных ролей
+        if (
+            # Администратор может редактировать:
+            # - только студентов
+            # - или свой собственный аккаунт
+            (current_user.role == UserRole.ADMIN and 
+            not (target_user.role == UserRole.STUDENT or 
+                (target_user.role == UserRole.ADMIN and current_user.user_id == target_user_id))) 
+            or
+            # Студент может редактировать только свой профиль
+            (current_user.role == UserRole.STUDENT and target_user.role != UserRole.STUDENT) 
+            or 
+            # Учитель может редактировать только учительский профиль
+            (current_user.role == UserRole.TEACHER and target_user.role != UserRole.TEACHER)
+        ):
             raise PermissionDeniedError()
+        
         return True
     
-    def _check_create_user(self: Self, current_user: User, **context) -> bool:
+    def _check_create_user(self: Self, current_user: UserTokenDataReadSchema, **context) -> bool:
         user_data = context.get('user_data')
         
         if current_user.role == UserRole.ADMIN and user_data.role != UserRole.STUDENT:
@@ -48,7 +66,7 @@ class PermissionsService:
         
         return True
 
-    def _check_delete_user(self, current_user: User, **context) -> bool:
+    def _check_delete_user(self, current_user: UserTokenDataReadSchema, **context) -> bool:
         target_user = context.get('target_user')
         if not target_user:
             raise ValueError("Target user is required for delete_user check")
--- school_site/apps/users/use_cases/update_user.py
+++ school_site/apps/users/use_cases/update_user.py
@@ -19,13 +19,14 @@ class UpdateUserUseCase(UpdateUserUseCaseProtocol):
         self.user_service = user_service
 
     async def __call__(self: Self, access_token: str, url_user_id: UUID, user_data: str, image: Optional[UploadFile] = None) -> UserWithPhotoReadSchema:
-        current_user = await self.auth_service.get_admin_user(access_token)
+        current_user = await self.auth_service.decode_acess_token(access_token)
         user_schema = UserUpdateRequestSchema(**json.loads(user_data))
 
         permission_service.check(
             "update_user",
             current_user=current_user,
-            target_user=user_schema
+            target_user=user_schema,
+            target_user_id=url_user_id
         )
         
         return await self.user_service.update_user_by_router(url_user_id, user_schema, image)
